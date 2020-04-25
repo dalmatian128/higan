1eb0b876e182c9e9fbc5daee69b7f07673fe0bdb
dalmatian <72544519+dalmatian128@users.noreply.github.com>
2020-04-25 17:25:25 +0900

    nall: Accept dynamic type in range.

--- nall/range.hpp
+++ nall/range.hpp
@@ -2,27 +2,29 @@
 
 namespace nall {
 
+template <typename T>
 struct range_t {
+  using utype = std::conditional_t<std::is_arithmetic_v<T>, T, s64>;
   struct iterator {
-    iterator(s64 position, s64 step = 0) : position(position), step(step) {}
-    auto operator*() const -> s64 { return position; }
+    iterator(utype position, utype step = 0) : position(position), step(step) {}
+    auto operator*() const -> utype { return position; }
     auto operator!=(const iterator& source) const -> bool { return step > 0 ? position < source.position : position > source.position; }
     auto operator++() -> iterator& { position += step; return *this; }
 
   private:
-    s64 position;
-    const s64 step;
+    utype position;
+    const utype step;
   };
 
   struct reverse_iterator {
-    reverse_iterator(s64 position, s64 step = 0) : position(position), step(step) {}
-    auto operator*() const -> s64 { return position; }
+    reverse_iterator(utype position, utype step = 0) : position(position), step(step) {}
+    auto operator*() const -> utype { return position; }
     auto operator!=(const reverse_iterator& source) const -> bool { return step > 0 ? position > source.position : position < source.position; }
     auto operator++() -> reverse_iterator& { position -= step; return *this; }
 
   private:
-    s64 position;
-    const s64 step;
+    utype position;
+    const utype step;
   };
 
   auto begin() const -> iterator { return {origin, stride}; }
@@ -31,21 +33,28 @@ struct range_t {
   auto rbegin() const -> reverse_iterator { return {target - stride, stride}; }
   auto rend() const -> reverse_iterator { return {origin - stride}; }
 
-  s64 origin;
-  s64 target;
-  s64 stride;
+  utype origin;
+  utype target;
+  utype stride;
 };
 
-inline auto range(s64 size) {
-  return range_t{0, size, 1};
+template <typename T>
+inline auto range(T size) {
+  return range_t<T>{0, size, 1};
 }
 
-inline auto range(s64 offset, s64 size) {
-  return range_t{offset, size, 1};
+template <typename T>
+inline auto range(T offset, T size) {
+  return range_t<T>{offset, size, 1};
+}
+template <typename T, typename U>
+inline auto range(T offset, U size) {
+  return range_t<T>{offset, static_cast<T>(size), 1};
 }
 
-inline auto range(s64 offset, s64 size, s64 step) {
-  return range_t{offset, size, step};
+template <typename T>
+inline auto range(T offset, T size, T step) {
+  return range_t<T>{offset, size, step};
 }
 
 }
